c15ab6f76df9d31e15abaa532f6fdb254af0f4ed
Code Sandbox <hello@codesandbox.io>
2022-05-16 16:58:06 +0000

    Deployment from Codesandbox.

---
+++ sw.js
@@ -0,0 +1,31 @@
+self.addEventListener('install', e => {
+
+})
+
+self.addEventListener('activate', async e => {
+
+})
+
+self.addEventListener('fetch', e => {
+  
+})
+
+
+
+self.addEventListener('push', function(event) {
+	if (event.data) {
+
+  } else {
+		
+	}
+})
+
+self.addEventListener('notificationclick', function(event) {
+  
+  
+})
+
+const showLocalNotification = (title, data, swRegistration) => {
+  
+  
+};
